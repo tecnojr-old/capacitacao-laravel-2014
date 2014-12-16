7e4280922976f4b4837c8661775ada57247952f4
Vagner do Carmo <vluzrmos@gmail.com>
2014-12-16 11:26:22 -0300

    Aula sobre migrations, criada a tabela e model Departamento

---
+++ app/database/migrations/2014_12_16_140012_create_departamentos_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDepartamentosTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('departamentos', function(Blueprint $table)
+		{
+			$table->increments('id');
+			
+			$table->string("descricao");
+			$table->string("sigla");
+			$table->string("area");
+
+			$table->timestamps();
+
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('departamentos');
+	}
+
+}
